4f10541944b8b07c817a7f4cec7f21858faeebe2
SuryaTechExpress <144881407+SuryaTechExpress@users.noreply.github.com>
2023-10-03 18:55:37 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -22,5 +22,21 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let num = 33;
 let stringNum = String(num);
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+// ********Operation**********
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = "Suraj";
+
+console.log(str1 + " " + str2);
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
