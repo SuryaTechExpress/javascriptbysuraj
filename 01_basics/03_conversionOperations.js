3c14fb5bc2556a8cf4f149b45255903a6b174df9
SuryaTechExpress <144881407+SuryaTechExpress@users.noreply.github.com>
2023-10-03 18:30:23 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "33abc";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+// null => 0
+
+let isLoggedIn = 10;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Suraj" => true
+
+let num = 33;
+let stringNum = String(num);
+console.log(stringNum);
+console.log(typeof stringNum);
