298896bd3beba8831fdaedc0151a964766a95c28
SuryaTechExpress <144881407+SuryaTechExpress@users.noreply.github.com>
2023-10-05 07:48:07 +0000

    iife in javascript

---
+++ 03_basics/06_iife.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB connected`);
+})();
+
+(
+    (name)=> console.log(`DB connected Two ${name}`)
+) ('Suraj')
