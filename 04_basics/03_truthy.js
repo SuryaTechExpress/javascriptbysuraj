ea9c690e9d986e909b15f7b718a960d1b6bf176c
SuryaTechExpress <144881407+SuryaTechExpress@users.noreply.github.com>
2023-10-06 12:47:03 +0000

    controll flow in js

---
+++ 04_basics/03_truthy.js
@@ -0,0 +1,48 @@
+let userEmail = "email.com" // true
+userEmail = "" // false
+userEmail = [] // true
+
+if(userEmail){
+    console.log("Got user email");
+}else{
+    console.log("Don't have user email");
+}
+
+
+// falsy value 
+/*
+=> false , 0 , -0  , BigInt , 0n , "" , null , undefined , NaN
+*/
+
+// truthy values
+// -> "0"  , 'false' , " " , [] , {} , function(){}
+// except all falsy values all are truthy value
+
+if(userEmail.length===0){
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length===0){
+    console.log(`Object is empty`)
+}
+
+// Nullish Coalescing Operator (??):  null undefined
+
+let val1;
+val1 = 5 ?? 10;
+console.log(val1);
+
+val1 = null ?? 10
+console.log(val1);
+
+val1 = undefined ?? 100
+console.log(val1);
+
+// Teranary operator 
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice<=80 ? console.log("Less than 80") : console.log("More than 80")
