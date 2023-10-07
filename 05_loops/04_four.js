9ed193b89766ecfe24fbd85b3d60d6a09f20d675
SuryaTechExpress <144881407+SuryaTechExpress@users.noreply.github.com>
2023-10-07 15:53:13 +0000

    .

--- 05_loops/04_four.js
+++ 05_loops/04_four.js
@@ -23,3 +23,25 @@ let myNum = [1,2,3,4]
 
 myNum = myNum.reduce((acc, currVal)=>acc+currVal,0)
 console.log(myNum);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+let allProdutPrice = shoppingCart.reduce((acc,item)=>acc+item.price,0)
+console.log(allProdutPrice);
