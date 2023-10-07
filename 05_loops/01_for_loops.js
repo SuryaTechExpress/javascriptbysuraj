b2539a98f3f5dc7091fdcd633ed6487774a67a7e
SuryaTechExpress <144881407+SuryaTechExpress@users.noreply.github.com>
2023-10-07 13:40:06 +0000

    loops in js

---
+++ 05_loops/01_for_loops.js
@@ -0,0 +1,12 @@
+// for loop
+let arr1 = ["Hello", 'How' , 'are' , 'you']
+
+for(let i = 0 ; i<arr1.length;i++){
+    console.log(arr1[i]);
+}
+
+for(let j = 1;j<=10;j++){
+    if(j==5) continue;
+    console.log(j);
+    
+}
